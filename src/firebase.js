64c4b95fbfd414e9280397187891477c794f6120
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-10-12 10:33:36 +0530

    Basic Setup Completed

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBawuteaN8_8-vZEHR8x2-RkBqxCODWtVE",
+    authDomain: "clone-d9805.firebaseapp.com",
+    projectId: "clone-d9805",
+    storageBucket: "clone-d9805.appspot.com",
+    messagingSenderId: "265919589255",
+    appId: "1:265919589255:web:6461794e56a8185ef34122",
+    measurementId: "G-WW3Q3884PH"
+  };
