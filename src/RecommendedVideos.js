a82a9c6cd77179dc41c66fef2651f7dbc2a2b5ee
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-10-13 14:33:35 +0530

    Main Components Styling Done

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,148 @@
 import React from 'react'
 import './RecommendedVideos.css'
+import VideoCard from './VideoCard'
 
 const RecommendedVideos = () => {
     return (
         <div className="recommendedVideos">
-            <h2>main page</h2>
+            <h2>Recommended </h2>
+            <div className="recommendedVideos__videos">
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard 
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+        </div>
         </div>
     )
 }
