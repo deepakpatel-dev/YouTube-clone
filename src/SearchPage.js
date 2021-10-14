658411495786d1f81017de06814652c71aa59cd9
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-10-14 11:38:26 +0530

    Search Functionality Working

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./SearchPage.css"
+import ChannelRow from './ChannelRow';
+import VideoRow from './VideoRow'
 import TuneOutlinedIcon  from '@material-ui/icons/TuneOutlined';
 
 const SearchPage = () => {
@@ -10,6 +12,105 @@ const SearchPage = () => {
             <h2>FLITER</h2>
             </div>
             <hr />
+            <ChannelRow 
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbLXDtbM0SZcLNDMCGG6fO3cOb-WVC8q-Wtw&usqp=CAU"
+            channel="Deepak Patel"
+            verified={true}
+            subs="569K" 
+            noOfVideos={382}
+            description="This channel is tribute to Maestro A R Rahman"
+            />
+            <hr />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
+            <VideoRow
+            subs="1.4M"
+            title="Now that you’ve included your keyword in the beginning"
+            views="2.3M Views"
+            timestamp="3 days ago"
+            // channelImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFHKYRgsmJxc_LOUlfkV4w9oAsQCzsr6HIpw&usqp=CAU"
+            channel="Deepak Patel"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWc9WjBwN1naWjea7cctXx5999p6KAYl-vRw&usqp=CAU"
+            description="Kuch  bhi"
+            />
         </div>
         
     )
